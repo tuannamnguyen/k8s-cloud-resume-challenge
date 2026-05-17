8ca31134a2170375f238c6c082feade8170f20ca
tuannamnguyen <tuannamnguyen290602@gmail.com>
2026-05-17 15:04:24 +0000

    update terraform to add IAM access entry for admin role

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -12,6 +12,8 @@ module "eks" {
   endpoint_public_access  = true
   endpoint_private_access = false
 
+  enable_cluster_creator_admin_permissions = true
+
   vpc_id     = var.vpc_id
   subnet_ids = var.private_subnet_ids
 }
