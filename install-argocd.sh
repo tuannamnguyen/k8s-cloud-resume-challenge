7e50957fa26556771f0248fd833e1f26788ac69d
tuannamnguyen <tuannamnguyen290602@gmail.com>
2026-05-23 06:11:50 +0000

    update common components

--- install-argocd.sh
+++
@@ -1,3 +0,0 @@
-kubectl create namespace argocd
-kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
